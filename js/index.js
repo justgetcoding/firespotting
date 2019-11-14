9e32f687144c0c126bdaf485b0aa02b134d0a119
Joe Janko <jjanko1284@gmail.com>
2019-11-13 22:54:07 -0500

    Finished map client implementation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
+var watchId = null;
+var mapCreated = false;
+var imagePreviewElement = null;
+
 $(function () {
+    $('#photo_input').on('change', onPhotoSelected)
+
+    imagePreviewElement = document.getElementById('imagePreview');
+
     if ("geolocation" in navigator) {
         navigator.geolocation;
-        $('#btnGetCoordinates').click(getGPSCoordinates);
+        getGPSCoordinates();
     }
     else {
         $('#warning').text('No access to geolocation data');
@@ -9,23 +17,83 @@ $(function () {
     }
 });
 
+function onPhotoSelected(event) {
+    var files = $('#photo_input').prop('files');
+    if (files.length > 0) {
+        let file = files[0];
+        if (file.type.startsWith('image/')) {
+            let img = document.createElement("img");
+            img.classList.add("obj");
+            img.file = file;
+            while (imagePreviewElement.firstChild) {
+                imagePreviewElement.removeChild(imagePreviewElement.firstChild);
+            }
+            imagePreviewElement.appendChild(img);
+
+            let reader = new FileReader();
+            reader.onload = (function (aImg) { return function (e) { aImg.src = e.target.result; }; })(img);
+            reader.readAsDataURL(file);
+        }
+    }
+}
+
 function getGPSCoordinates() {
     let numVars = arguments.length;
 
     let geo_options = {
         enableHighAccuracy: true,
-        maximumAge: 30000,
+        maximumAge: 0,
         timeout: 27000
     };
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-        setTimeout(displayCoordinates(position.coords), 0);
-    }, null, geo_options);
+    watchId = navigator.geolocation.watchPosition(
+        function (position) {
+            onGotCoordinates(position.coords)
+        },
+        null,
+        geo_options);
+}
+
+function onGotCoordinates(coords) {
+    displayReading(coords);
+    displayCoordinates(coords);
+    if (!mapCreated) {
+        createMap(coords.latitude, coords.longitude);
+        mapCreated = true;
+    }
 }
 
 function displayCoordinates(coords) {
     $('#latitude').text(coords.latitude);
     $('#longitude').text(coords.longitude);
     $('#accuracy').text(coords.accuracy);
+}
+
+function displayReading(coords) {
+    $('#readingsList').append(`<li>Reading taken at: ${new Date(Date.now())}. Lat:${coords.latitude} Long:${coords.longitude} Acc:${coords.accuracy}</li>`)
+    let theNumber = $("#readingsList li").length
+    while (theNumber > 5) {
+        $("#readingsList li").first().detach()
+        theNumber = $("#readingsList li").length
+    }
+    $('#readingsCount').text(theNumber)
+}
+
+function formatDate(date) {
+    return date.getHours() + ':'
+}
+
+function createMap(lat, long) {
+    var mymap = L.map('mapid').setView([lat, long], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(mymap);
+
+    L.marker([lat, long]).addTo(mymap);
 }
 
